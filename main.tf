e4c118db9e3e9611dc105bf0947062039cf00e9a
“Mohideen.Hajamohideen” <Mohideen.Hajamohideen@outlook.com>
2021-02-03 23:40:07 +0000

    azure vm deployment

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "google_compute_instance" "mohivm" {
 }
 
 variable "prefix" {
-  default = "tfvmex"
+  default = "uldemo"
 }
 
 resource "azurerm_resource_group" "main" {
@@ -86,7 +86,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix       = ["10.0.2.0/24"]
+  address_prefix       = "10.0.1.0/24"
 }
 
 resource "azurerm_network_interface" "main" {
