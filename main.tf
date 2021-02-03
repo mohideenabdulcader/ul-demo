790070a1f0df91807b54ff24b84065bef52c26e8
“Mohideen.Hajamohideen” <Mohideen.Hajamohideen@outlook.com>
2021-02-03 11:29:34 +0000

    Updated User-data

--- main.tf
+++ main.tf
@@ -31,6 +31,10 @@ resource "google_compute_instance" "mohivm" {
       gnupg-agent \
       software-properties-common -y
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/debian \
+      $(lsb_release -cs) \
+      stable"
     sudo apt-get update
     sudo apt-get install docker-ce docker-ce-cli containerd.io -y
     sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
